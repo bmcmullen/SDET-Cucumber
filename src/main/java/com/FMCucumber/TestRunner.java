d31bd7de50662e812e3a0bb212a8493a6af7d39d
Brian <brian.mcmullen@gmail.com>
2018-12-21 14:43:48 -0500

    Update for Runner

--- src/main/java/com/FMCucumber/TestRunner.java
+++ src/main/java/com/FMCucumber/TestRunner.java
@@ -3,8 +3,11 @@ package com.FMCucumber;
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
-//@CucumberOptions(features = "features")
-@CucumberOptions(features = "features/InvalidLoginDDT.feature")
+@CucumberOptions(features = "features")
+//@CucumberOptions(features = "features/InvalidLoginDDT.feature")
+//@CucumberOptions(features = "features" glue = "com.FMCucmber", 
+//					dryRun=false, 
+//					plugin = {"pretty", "html:target/cucumber.html"})
 public class TestRunner extends AbstractTestNGCucumberTests {
 
 }
